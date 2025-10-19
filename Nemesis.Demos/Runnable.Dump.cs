689e46696dc0dd9ac6032d6e8174afc3f0d708aa
Michał Bryłka <michal.brylka@gmail.com>
2025-10-19 23:16:39 +0200

    Render exceptions

--- Nemesis.Demos/Runnable.Dump.cs
+++ Nemesis.Demos/Runnable.Dump.cs
@@ -44,8 +44,8 @@ public partial class Runnable
         if (obj is XNode xml)
             return new Markup(demo.HighlighterFactory.GetSyntaxHighlighter(Language.Xml).GetHighlightedMarkup(xml.ToString()));
 
-        if (obj is Exception ex)
-            AnsiConsole.WriteException(ex, ExceptionFormats.ShortenEverything | ExceptionFormats.ShowLinks);
+        if (obj is Exception exception)
+            AnsiConsole.WriteException(exception, ExceptionFormats.ShortenEverything | ExceptionFormats.ShowLinks);
 
         static Markup GetFormattableMarkup(IFormattable formattable, string? format, string? style = null) =>
             new(
